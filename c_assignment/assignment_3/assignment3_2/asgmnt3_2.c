2543b2ad69b296ebbaa396f81bfd91d1b04ba54f
patnool-muskan <pmuskan2445@gmail.com>
2025-09-12 13:02:33 +0530

    Add files via upload

---
+++ c_assignment/assignment_3/assignment3_2/asgmnt3_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int Add(int a, int b) { return a + b; }
+int Sub(int a, int b) { return a - b; }
+int Mul(int a, int b) { return a * b; }
+int Div(int a, int b) { return b != 0 ? a / b : 0; }
+int Mod(int a, int b) { return b != 0 ? a % b : 0; }
+
+int main() {
+    int x = 20, y = 6;
+    printf("Add = %d\n", Add(x, y));
+    printf("Sub = %d\n", Sub(x, y));
+    printf("Mul = %d\n", Mul(x, y));
+    printf("Div = %d\n", Div(x, y));
+    printf("Mod = %d\n", Mod(x, y));
+    return 0;
+}
+
