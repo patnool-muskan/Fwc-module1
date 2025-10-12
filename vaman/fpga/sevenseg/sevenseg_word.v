f4a12746beddba1c78e8f7ec9613b7d02cd566e8
patnool-muskan <pmuskan2445@gmail.com>
2025-10-12 20:48:21 +0530

    Create sevenseg_word.v

---
+++ vaman/fpga/sevenseg/sevenseg_word.v
@@ -0,0 +1,16 @@
+module helloworldfpga(
+                 output a,
+                 output b,
+                 output c,
+             output d,
+               output e,
+              output f,
+               output g
+
+);
+
+reg [6:0] gpio_out=7'b1001100;
+
+assign {a, b,c,d,e,f,g}=gpio_out[6:0];
+endmodule
+//end of the module
